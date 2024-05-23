3c243d005b7cc1f45c7f774031eeea97781638fa
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2024-05-23 18:31:42 +0200

    fix(frontend): replace reference to 'AppConfig' by 'ConfigService'

--- frontend/app/shared/services/config.service.ts
+++ frontend/app/shared/services/config.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
-import { AppConfig } from '@geonature_config/app.config';
+import { ConfigService as GnConfigService } from '@geonature/services/config.service';
 import { ModuleConfig } from '../../module.config';
 
 @Injectable()
 export class ConfigService {
 
   constructor(
-    private http: HttpClient,
+    public config: GnConfigService,
   ) {}
 
   getModuleTitle() {
@@ -20,15 +19,15 @@ export class ConfigService {
   }
 
   getAppUrl() {
-    return `${AppConfig.URL_APPLICATION}`;
+    return `${this.config.URL_APPLICATION}`;
   }
 
   getBackendUrl() {
-    return `${AppConfig.API_ENDPOINT}`;
+    return `${this.config.API_ENDPOINT}`;
   }
 
   getModuleBackendUrl() {
-    return `${AppConfig.API_ENDPOINT}/${ModuleConfig.module_code.toLowerCase()}`;
+    return `${this.config.API_ENDPOINT}/${ModuleConfig.module_code.toLowerCase()}`;
   }
 
   getFrontendModuleUrl() {
@@ -36,14 +35,14 @@ export class ConfigService {
   }
 
   getTaxHubBackendUrl() {
-    return `${AppConfig.API_TAXHUB}`;
+    return `${this.config.API_TAXHUB}`;
   }
 
   getTaxHubFrontendUrl() {
-    return `${AppConfig.API_TAXHUB}`.replace('/api', '/');
+    return `${this.config.API_TAXHUB}`.replace('/api', '/');
   }
 
   getPriorityFloraBackendUrl() {
-    return `${AppConfig.API_ENDPOINT}/${ModuleConfig.module_code_pf.toLowerCase()}`;
+    return `${this.config.API_ENDPOINT}/${ModuleConfig.module_code_pf.toLowerCase()}`;
   }
 }
